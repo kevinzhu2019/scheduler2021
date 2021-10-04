299c909ec76c200290350a1cc7732ff04adea13b
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2021-10-04 11:58:36 -0400

    used axios to fetch days data from api server instead of hardcoded data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 import "./Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment/index";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -72,6 +55,14 @@ const appointments = [
 export default function Application(props) {
 
   const[day, setDay] = useState("Monday");
+  const[days, setDays] = useState([]);
+
+  useEffect(() => {
+    let url = "/api/days";
+    axios
+    .get(url)
+    .then(response => setDays([...response.data]));
+  }, []);
 
   const appointmentList = appointments.map(appointment => {
     return (
